afff37ef26f935f646b08823ab493507cd01ac72
nahid96 <nahid.nawaz96@gmail.com>
2019-07-22 23:56:40 +0600

    master - initial commit and category, product, customer, supplier model added

---
+++ SmallBusinessManagementSystemWebApp/SmallBusinessManagementSystemWebApp.Models/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallBusinessManagementSystemWebApp.Models.Models
+{
+    public class Product
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int ReorderLevel { get; set; }
+        public string Description { get; set; }
+    }
+}
